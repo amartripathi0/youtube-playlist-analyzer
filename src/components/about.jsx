dfeff65d5c6a96ae9f13859cfb37d8ac5e3ef073
amartripathi0 <theamartripathi@gmail.com>
2024-09-27 16:44:45 +0530

    fix: about para alignment with navbar buttons

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,6 +1,6 @@
 function About() {
   return (
-    <p className="absolute bottom-24 p-3 mx-40 w-4/5 rounded-md  max-sm:mt-6  bg-slate-200 font-normal max-sm:text-base text-neutral-950">
+    <p className="absolute bottom-24 p-3 mx-40  rounded-md  max-sm:mt-6  bg-slate-200 font-normal max-sm:text-base text-neutral-950">
       YouTube Playlist Analyzer evaluates your playlist&apos;s total duration,
       then breaks down viewing times at speeds of 1.25x, 1.5x, 1.75x, and 2x.
       Streamline your viewing experience and optimize your binge-watching
