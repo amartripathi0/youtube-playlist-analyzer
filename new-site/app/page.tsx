8dbc5e27ab42682a9cdc98ee9894702461ea02f0
amartripathi0 <theamartripathi@gmail.com>
2024-10-02 14:47:19 +0530

    fix: navbar position

--- new-site/app/page.tsx
+++ new-site/app/page.tsx
@@ -5,9 +5,7 @@ import About from "./components/about";
 
 export default function Home() {
   return (
-    <div className="flex flex-col gap-3 relative w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100 ">
-      {/* <ToastContainer position="bottom-right" className={"bottom-10 text-sm"} /> */}
-      {/* <Analytics /> */}
+    <div className="flex flex-col gap-3 relative py-6 w-screen min-h-screen bg-gradient-to-r from-rose-100 to-teal-100 ">
       <NavBar />
       <HomePage />
       <About />
