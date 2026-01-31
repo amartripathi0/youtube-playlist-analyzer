c9b6fe11cf756ccfd07896b398e31572ab6d3e6c
amartripathi0 <theamartripathi@gmail.com>
2026-01-31 20:59:30 +0530

    feat(types): add playlist insights and sorting types

--- new-site/types/index.ts
+++ new-site/types/index.ts
@@ -1,9 +1,33 @@
 export interface VidPlaybackTimeInDiffSpeedType {
   [key: number]: { hr: number; min: number; sec: number };
-} 
+}
 
 export interface TotalTimeDurationType {
   hr: number;
   min: number;
   sec: number;
 }
+
+export interface VideoMetadata {
+  id: string;
+  title: string;
+  thumbnail: string;
+  duration: string;
+  definition: "hd" | "sd";
+  caption: boolean;
+}
+
+export interface PlaylistInsights {
+  totalSec: number;
+  avgDuration: { hr: number; min: number; sec: number };
+  hdPercentage: number;
+  captionPercentage: number;
+}
+
+export interface TranscriptItem {
+  text: string;
+  duration: number;
+  offset: number;
+}
+
+export type SortOrder = "default" | "shortest" | "longest" | "az" | "za";
